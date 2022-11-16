f32e8e6be46b5420e5dcbe6cdb1202c827d8d9e6
LeonardoRCosta <rm94286@fiap.com.br>
2022-11-16 18:17:48 -0300

    Criadas as funcionalidades de login e logout.

--- ProjetoAgenda/src/controllers/loginController.js
+++ ProjetoAgenda/src/controllers/loginController.js
@@ -1,10 +1,11 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
-  res.render('login');
+  if(req.session.user) return res.render('login-logado');
+  return res.render('login');
 };
 
-exports.register = async function(req, res) {
+exports.register = async function (req, res) {
   try {
     const login = new Login(req.body);
     await login.register();
@@ -19,4 +20,32 @@ exports.register = async function(req, res) {
     console.log(e);
     return res.render('404');
   }
+};
+
+exports.login = async function (req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+    if (login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(() => res.redirect(`/login`));
+      return;
+    }
+    if (!login.user) {
+      req.flash('errors', login.errors);
+      req.session.save(() => res.redirect(`/login`));
+      return;
+    }
+    req.flash('success', 'Logado com sucesso!');
+    req.session.user = login.user;
+    req.session.save(() => res.redirect(`/login`));
+  } catch (e) {
+    console.log(e);
+    return res.render('404');
+  }
+};
+
+exports.logout = function (req, res) {
+  req.session.destroy();
+  res.redirect('/');
 };
